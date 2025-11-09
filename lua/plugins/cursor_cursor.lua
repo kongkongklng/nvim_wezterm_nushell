e63b9961e4c9c14b6a4037ad7a8edd53243bdf1c
kongklng <kongkongklng697@gmail.com>
2025-11-09 22:41:22 +0800

    添加丝滑的光标

---
+++ lua/plugins/cursor_cursor.lua
@@ -0,0 +1,15 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    legacy_computing_symbols_support = true,
+    cursor_color = "#42b983",
+    stiffness = 0.3,
+    trailing_stiffness = 0.1,
+    time_interval = 7,
+    damping = 0.5,
+    trailing_exponent = 5,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
+  },
+}
