407faffa7350079f6a802e89f59702fef9595663
kongklng <kongkongklng697@gmail.com>
2025-11-11 05:36:21 +0800

    添加Yazi 添加官网的一系列插件

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+-- ---------- 正常模式 ---------- ---
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+-- ---------- 插入模式 ---------- ---
+keymap.set("i", "jk", "<ESC>")
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
